f5bd61a6154ae9d79668beb2ca58716bd89a117c
trapido02 <trapido02.2@gmail.com>
2024-07-31 20:42:52 +0200

    Added: GLFW and updated CMakeLists
    Also removed most of the gitignore

--- source/main.cpp
+++ source/main.cpp
@@ -1,7 +1,38 @@
+#include <GLFW/glfw3.h>
+
 #include <iostream>
 
 int main()
 {
-	std::cout << "Hello, world!" << std::endl;
-	return 0;
+    GLFWwindow* window;
+
+    if (!glfwInit())
+        return -1;
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 5);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    window = glfwCreateWindow(720*16/9, 720, "Aurora", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    while (!glfwWindowShouldClose(window))
+    {
+        glClear(GL_COLOR_BUFFER_BIT);
+        glClearColor(0.1f, 0.3f, 0.2f, 1.0f);
+
+        // Render here
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
 }
