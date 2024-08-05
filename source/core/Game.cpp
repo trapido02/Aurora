459ae06574152a1d8a4f84521d88b7e2f2f85788
trapido02 <trapido02.2@gmail.com>
2024-08-05 02:10:24 +0200

    Created: Triangle!
    Added basic code for creating a triangle.

--- source/core/Game.cpp
+++ source/core/Game.cpp
@@ -1,6 +1,24 @@
 #include "Game.h"
 
 #include <iostream>
+#include <string>
+
+const char* vertexShaderSource = R"(
+#version 450 core
+layout (location = 0) in vec3 aPos;
+void main()
+{
+	gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);
+}
+)";
+const char* fragmentShaderSource = R"(
+#version 450 core
+out vec4 FragColor;
+void main()
+{
+	FragColor = vec4(0.6f, 0.4f, 1.0f, 1.0f);
+}
+)";
 
 namespace Core {
 
@@ -12,10 +30,57 @@ namespace Core {
 
 	void Game::Run()
 	{
+		// Compile shaders
+		unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+		unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+
+		glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+		glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+
+		glCompileShader(vertexShader);
+		glCompileShader(fragmentShader);
+
+		// Link shaders
+		unsigned int shaderProgram = glCreateProgram();
+		glAttachShader(shaderProgram, vertexShader);
+		glAttachShader(shaderProgram, fragmentShader);
+		glLinkProgram(shaderProgram);
+
+		// Delete shaders after they are linked to the shader program
+		glDeleteShader(vertexShader);
+		glDeleteShader(fragmentShader);
+
+		// Setup vertices
+		float vertices[] = {
+			-0.5f, -0.5f, 0.0f,
+			 0.5f, -0.5f, 0.0f,
+			 0.0f, 0.5f, 0.0f
+		};
+
+		// Create the VBO and VAO
+		unsigned int VBO, VAO;
+		glGenVertexArrays(1, &VAO);
+		glGenBuffers(1, &VBO);
+
+		glBindVertexArray(VAO);
+		glBindBuffer(GL_ARRAY_BUFFER, VBO);
+
+		glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+		glEnableVertexAttribArray(0);
+
+		glBindBuffer(GL_ARRAY_BUFFER, 0);
+		glBindVertexArray(0);
+
 		while (m_IsRunning)
 		{
 			m_Renderer.Clear();
 			m_Renderer.ClearColor(0.1f, 0.3f, 0.2f, 1.0f);
+
+			glUseProgram(shaderProgram);
+			glBindVertexArray(VAO);
+			glDrawArrays(GL_TRIANGLES, 0, 3);
 
 			m_Window->OnUpdate();
 		}
