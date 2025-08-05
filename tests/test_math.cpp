39e3bfba1e04a9e76522e34eda1670c69b9b2877
Trapido02 <trapido02.2@gmail.com>
2025-08-05 19:30:05 +0200

    Removed test failing

--- tests/test_math.cpp
+++ tests/test_math.cpp
@@ -6,5 +6,4 @@ TEST_CASE("Basic math sanity checks", "[math]")
     REQUIRE(2 + 2 == 4);
     REQUIRE(std::sqrt(9.0) == Catch::Approx(3.0));
     REQUIRE(std::pow(2.0, 3.0) == Catch::Approx(8.0));
-    FAIL("This test should fail");
 }
